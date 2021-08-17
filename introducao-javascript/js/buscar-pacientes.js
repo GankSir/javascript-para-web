60998c4346e408ab16aa355aa584b1ed096175f9
GankSir <sirgank13@gmail.com>
2021-08-17 11:17:40 -0300

    finishe course JS web

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -3,14 +3,15 @@ botaoImportar.addEventListener("click", function () {
   console.log("Buscando pacientes")
   //XHMHttpRequest responsavel por fazer requisicoes no JS
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes"); //abrir a conexao e configurar
 
+  //escuta a resposta
   xhr.addEventListener("load", function () {
     var erroAjax = document.querySelector("#erro-ajax");
     if (xhr.status == 200) {
 
-      erroAjax.classList.add("invisivel")
-      var resposta = xhr.responseText;
+      erroAjax.classList.add("invisivel");
+      var resposta = xhr.responseText; // resposta sempre volta com response.text
       var pacientes = JSON.parse(resposta);
       //para cada paciente voce pega esse paciente e chama a função anonima
       pacientes.forEach(function (paciente) {
@@ -24,7 +25,7 @@ botaoImportar.addEventListener("click", function () {
 
   });
 
- xhr.send()
+ xhr.send() //envia a requisão
 });
 
 //typeof:tipo de resposta
